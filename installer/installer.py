a635a4b2536246166ccaf350da57e65618044dc6
Philipp Omenitsch <philipp.omenitsch@gmail.com>
2022-11-23 13:09:29 +0100

    fix yml dumping

--- installer/installer.py
+++ installer/installer.py
@@ -3,7 +3,8 @@ import menu
 import utils
 from nginxInstaller import NginxInstaller
 
-ymlConfigFilePath = "/etc/openpdfsign/config.yml"
+#ymlConfigFilePath = "/etc/openpdfsign/config.yml"
+ymlConfigFilePath = "config.yml"
 
 # configFile = "tests/nginxconfig/multiple.com"
 # nginxConfigs = utils.getNginxConfigs("/etc/nginx/sites-available")
@@ -16,17 +17,19 @@ for nginxConfig in nginxConfigs:
     installers.append(installer)
 selection, infos = menu.installMenu(installers)
 
+services = []
 for i in range(len(infos)):
     installer = installers[i]
-    installer.insertBlocks([infos[i][s]["index"] for s in selection[i]])
+    service = [infos[i][s] for s in selection[i]]
+    services.extend(service)
+    installer.insertBlocks([s["index"] for s in service])
     nginxDump = nginxparser.dumps(installer.parsed)
     print(nginxDump)
 
 # save all to yml file
-utils.saveConfigToYml(selection, ymlConfigFilePath)
+utils.saveConfigToYml(services, ymlConfigFilePath)
 utils.startServerAsService(ymlConfigFilePath)
 
 # which java
 
-# https://github.com/rtr-nettest/rmbt-server/blob/master/rmbt.service
 # install via apt-get https://earthly.dev/blog/creating-and-hosting-your-own-deb-packages-and-apt-repo/+
